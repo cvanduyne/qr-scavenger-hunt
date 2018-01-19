b239c02c0a34e62b1cb6f95719642150935b8e39
cvanduyne <35565350+cvanduyne@users.noreply.github.com>
2018-01-18 20:35:34 -0500

    Update game-logic.js

--- js/game-logic.js
+++ js/game-logic.js
@@ -2,8 +2,18 @@
 
 var gameLogic = {};
 
+var data = [
+  {unlocked: false, name: "Bob", age: 36, description: "sits and watches TV"},
+  {unlocked: false, name: "Mary", age: 36, description: "Has lots of cats"}
+]
+gameLogic.OnFoundId = function( id )
+{
+  console.log("Handling id: "+id);  
+}
 gameLogic.HandleScan = function( value )
 {
-  console.log("Handling scan: "+value);
-
+  var s = value.split(" ")
+  var i = parseInt( s[s.length - 1] );
+  
+  this.OnFoundId( i );
 }
