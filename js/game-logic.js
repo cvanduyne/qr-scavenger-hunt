a6ffd9a07e7b7fe22504c3ec22eba511b3490f4b
cvanduyne <35565350+cvanduyne@users.noreply.github.com>
2018-01-18 21:27:34 -0500

    Update game-logic.js

--- js/game-logic.js
+++ js/game-logic.js
@@ -6,6 +6,59 @@ gameLogic.data = [
   {unlocked: false, image: "FOO", name: "Bob", age: 36, description: "sits and watches TV", container: {main: null}},
   {unlocked: false, image: "BAR", name: "Mary", age: 36, description: "Has lots of cats", container: {main: null}}
 ]
+
+gameLogic.Start = function()
+{ 
+  this.CreateCards( document.getElementById("cardContainer") );
+}
+
+gameLogic.CreateCards = function( parentElement )
+{
+  for (var i=0; i < this.data.length; i++)
+  {
+    var s = document.createElement("span");
+    this.data[i].container.main = s;
+    this.CreateCard(s, i);
+    parentElement.appendChild(s);
+  }
+  this.UpdateCards();
+}
+gameLogic.UpdateCard = function(id)
+{
+  var container = this.data[id].container;
+  container.image.innerHTML = this.data[id].image;
+  container.name.innerHTML = this.data[id].name;
+  container.age.innerHTML = this.data[id].age;
+  container.description.innerHTML = this.data[id].description;
+}
+gameLogic.UpdateCards = function()
+{
+  for (var i=0; i < this.data.length; i++)
+  {
+    this.UpdateCard(i);
+  }  
+}
+gameLogic.OnFoundId = function( id )
+{
+  console.log("Handling id: "+id);  
+  
+}
+gameLogic.HandleScan = function( value )
+{
+  // Turn the scanned value into a zero based index referring to the data items
+  try {
+    var s = value.split(" ")
+    var i = parseInt( s[s.length - 1] ) - 1;
+    
+    if (i >= 0 && i < this.data.length)
+    {
+      this.OnFoundId( i );
+    }
+  }
+  catch( err ) {
+    console.log("Got error parsing scan: "+err); 
+  }
+}
 gameLogic.CreateCard = function( parentElement, id )
 {
   // creates a <table> element and a <tbody> element
@@ -60,51 +113,5 @@ gameLogic.CreateCard = function( parentElement, id )
   // sets the border attribute of tbl to 2;
   tbl.setAttribute("border", "2");  
 }
-gameLogic.CreateCards = function( parentElement )
-{
-  for (var i=0; i < this.data.length; i++)
-  {
-    var s = document.createElement("span");
-    this.data[i].container.main = s;
-    this.CreateCard(s, i);
-    parentElement.appendChild(s);
-  }
-  this.UpdateCards();
-}
-gameLogic.UpdateCard = function(id)
-{
-  var container = this.data[id].container;
-  container.image.innerHTML = this.data[id].image;
-  container.name.innerHTML = this.data[id].name;
-  container.age.innerHTML = this.data[id].age;
-  container.description.innerHTML = this.data[id].description;
-}
-gameLogic.UpdateCards = function()
-{
-  for (var i=0; i < this.data.length; i++)
-  {
-    this.UpdateCard(i);
-  }  
-}
-gameLogic.OnFoundId = function( id )
-{
-  console.log("Handling id: "+id);  
-  
-}
-gameLogic.HandleScan = function( value )
-{
-  // Turn the scanned value into a zero based index referring to the data items
-  try {
-    var s = value.split(" ")
-    var i = parseInt( s[s.length - 1] ) - 1;
-    
-    if (i >= 0 && i < this.data.length)
-    {
-      this.OnFoundId( i );
-    }
-  }
-  catch( err ) {
-    console.log("Got error parsing scan: "+err); 
-  }
-}
-gameLogic.CreateCards( document.getElementById("cardContainer") );
+
+gameLogic.Start();
